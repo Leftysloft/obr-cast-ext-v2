24993650217401c796c97066d05c2d3127422f43
Leftysloft <patrickbglose@gmail.com>
2024-09-02 15:11:13 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,14 @@
-import path from "path";
-import fs from "fs";
+// vite.config.js
+import { resolve } from "path";
+import { defineConfig } from "vite";
 
-function getHtmlEntries() {
-  const pagesDir = path.resolve(__dirname, "");
-  const entries = {};
-
-  // Read all files in the directory
-  const files = fs.readdirSync(pagesDir);
-
-  // Filter out HTML files
-  const htmlFiles = files.filter((file) => file.endsWith(".html"));
-
-  // Create entries for each HTML file
-  htmlFiles.forEach((file) => {
-    const name = path.basename(file, ".html");
-    entries[name] = path.resolve(pagesDir, file);
-  });
-
-  return entries;
-}
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        nested: resolve(__dirname, "src/character-data.html"),
+      },
+    },
+  },
+});
