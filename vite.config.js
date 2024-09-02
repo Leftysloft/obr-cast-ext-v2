6424660e0717181d33fb6a4991f532b11fb8fe1d
Leftysloft <patrickbglose@gmail.com>
2024-09-02 15:13:11 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,8 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "index.html"),
-        nested: resolve(__dirname, "src/character-data.html"),
+        main: resolve(__dirname, "./index.html"),
+        nested: resolve(__dirname, "./src/character-data.html"),
       },
     },
   },
