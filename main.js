fee22587c6bc73a6ac4a98683c7beba9bf67d51a
Leftysloft <patrickbglose@gmail.com>
2024-09-06 18:31:03 -0400

    folder correction

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ import OBR from "@owlbear-rodeo/sdk";
 //import { setupContextMenu } from "./src/popup";
 //import { imageFilter } from "./src/imageUrl"; //Current file for getting the image url from all characters on a scene.
 //import { imageFilter } from "./src/imageFetch"; //This is the original file for retrieving character image urls.
-import { imageFilter } from "./src/imageUrl";
 import { ID } from "./src/constants";
 import { setupContextMenu } from "./contextMenu";
 
