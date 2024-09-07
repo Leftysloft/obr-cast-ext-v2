a99c903659860a010f52b8adc91deb3a39dadfe1
Leftysloft <patrickbglose@gmail.com>
2024-09-07 17:45:05 -0400

    Update settings-main.js

--- settings/settings-main.js
+++ settings/settings-main.js
@@ -3,5 +3,10 @@ import "../style.css";
 import "../sheetList";
 
 OBR.onReady(() => {
+  document.querySelector("#app").innerHTML = `
+    <div>
+      <ul id="sheet-list"></ul>
+    </div>
+  `;
   setupSheetList(document.querySelector("#sheet-list"));
 });
